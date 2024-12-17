2af6eba7bd06ef2aacaac1d1cfa1d2f9ffd9edc1
Gabriele Dellino <gdellino37@gamil.com>
2024-12-17 11:27:19 +0100

    user story 4 done

--- src/main/java/it/aulab/progetto_finale_gabriele_dellino/config/SecurityConfig.java
+++ src/main/java/it/aulab/progetto_finale_gabriele_dellino/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
             .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/register/**").permitAll()
             .requestMatchers("/admin/dashboard","/categories/create","/categories/edit{id}","/categories/update/{id}","/categories/delete/{id}").hasRole("ADMIN")
             .requestMatchers("/revisor/dashboard", "revisor/detail/{id}", "/accept").hasRole("REVISOR")
-            .requestMatchers("/register","/","/articles","/images/**","/articles/detail/**","/categories/search/{id}","/search/{id}").permitAll()
+            .requestMatchers("/register","/","/articles","/images/**","/articles/detail/**","/categories/search/{id}","/search/{id}","/articles/search").permitAll()
             .anyRequest().authenticated()
         ).formLogin(form -> form.loginPage("/login")
             .loginProcessingUrl("/login")
