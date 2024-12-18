f450c57ee6cf3aa69e3f34d5a5c796d54266acdd
Gabriele Dellino <gdellino37@gamil.com>
2024-12-18 17:50:42 +0100

    check

--- sql/createImageTable.sql
+++ sql/createImageTable.sql
@@ -1,6 +1,6 @@
 create TABLE images(
     id BIGINT auto_increment PRIMARY KEY,
     path VARCHAR(255) NOT NULL,
-    artile_id BIGINT,
-    FOREIGN KEY (artile_id) REFERENCES articles(id)
+    article_id BIGINT,
+    FOREIGN KEY (article_id) REFERENCES articles(id)
 );
